06d94844cd20df25969905f1826426da2909b80f
jnadyr <jnadyr@gmail.com>
2023-11-24 19:40:26 -0300

    tudo ok com ansible terraform e gitclone

--- infra/main.tf
+++ infra/main.tf
@@ -8,34 +8,34 @@ terraform {
   required_version = ">= 1.2.0"
 }
 provider "aws" {
-#buscando o valor da regiao aws
+  # buscando o valor da regiao aws
   region  = var.regiao_aws
 }
- resource "aws_instance" "app_server" {
-  ami           = "ami-0230bd60aa48260c6"
-#buscando o valor da instancia
-  instance_type = var.instancia
-#buscando o valor da chave
-  key_name = var.chave
-#vinculando a instância ao security group criado em grupo de segurança
-  vpc_security_group_ids = [aws_security_group.acesso_geral.id]
-  #vpc_security_group_ids = [aws_security_group.acesso_geral_prod.id]
+resource "aws_instance" "app_server" {
+  ami            = "ami-0fc5d935ebf8bc3bc"
+  # buscando o valor da instancia
+instance_type = var.instancia
+# buscando o valor da chave
+key_name = var.chave
+# vinculando a instância ao security group criado em grupo de segurança
+vpc_security_group_ids = [aws_security_group.acesso_geral.id]
 
   tags = {
     Name = "Terraform Ansible Phyton"
   }
- }
-#criando o recurso da chave publica no arquivo main.tf do terraform gerada pelo ssh-keygen.
- resource "aws_key_pair" "chave" {
+}
+# criando o recurso da chave publica no arquivo main.tf do terraform
+# gerada pelo ssh-keygen.
+resource "aws_key_pair" "chave" {
   key_name = var.chave
   public_key = file("${var.chave}.pub")
- }
-#passamos a chave atraves do file para não expor a chave publica na internet!
-#aqui é a chave publica que temos que passar!
+}
+# passamos a chave atraves do file para não expor a chave publica na internet!
+# aqui é a chave publica que temos que passar!
 
- output "IP_publico_da_maquina" {
-  value = aws_instance.app_server.public_ip 
-#aqui segue-se o caminho da maquina!
-#caminho da maquina: aws_instance . app_server . public_ip
+output "IP_publico_da_maquina" {
+  value = aws_instance.app_server.public_ip
+ # aqui segue-se o caminho da maquina!
+ # caminho da maquina: aws_instance . app_server . public_ip
  }
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,4 @@
-#declaração das variaveis a serem utilizadas pelo terraform nos ambientes distintos de dev e prod
+# declaração das variaveis a serem utilizadas pelo terraform nos ambientes distintos de dev e prod
 variable "regiao_aws" {
   type = string
     }
